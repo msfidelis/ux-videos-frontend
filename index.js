426a512f9be932c3b873efcb650057aacbccb6f3
Matheus Fidelis <msfidelis@outlook.com>
2018-01-20 00:41:46 -0200

    consuming list api

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 'use strict';
 
+const api = require('./lib/api');
+
+const exphbs = require('express-handlebars');
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/static')); 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.use(express.static(path.join(__dirname, 'static')));
+//app.enable('view cache');
 
-app.listen(process.env.PORT || 3000);
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+
+    api.paginate().then(data => {
+        console.log(data);
+        res.render('home', data);
+    });
+
+});
+
+app.listen(process.env.PORT || 4000);
