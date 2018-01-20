abd4dbd488e5e4f0b1fb3f4e8f7a99a5bdfe1c4c
Matheus Fidelis <msfidelis@outlook.com>
2018-01-20 01:21:51 -0200

    helpers and pagination

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const api = require('./lib/api');
+const helpers = require('./lib/helpers');
 
 const exphbs = require('express-handlebars');
 const express = require('express');
 const path = require('path');
 const app = express();
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: helpers }));
 app.use(express.static(path.join(__dirname, 'static')));
 //app.enable('view cache');
 
@@ -15,7 +16,12 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
 
-    api.paginate().then(data => {
+    const query = {
+        page : req.query.page || 1
+    };
+
+    api.paginate(query).then(data => {
+        console.log(query);
         console.log(data);
         res.render('home', data);
     });
